5581b2fdbe69de23804c776bdac07c0412cbc673
Ty <tylertorresp@gmail.com>
2021-11-28 14:00:43 -0500

    Merging in place two sorted lists

--- educative/ds_interviews_python/merge_sorted_lists.py
+++ educative/ds_interviews_python/merge_sorted_lists.py
@@ -26,6 +26,38 @@ def merge(lst1, lst2):
     return result
 
 
+# O(N * M) T | O(M) S
+# Further reduced to
+    # O(N^2) T | O(M) S
+def merge_inplace(lst1, lst2):
+
+    idx1, idx2 = 0, 0
+
+    while idx1 < len(lst1) and idx2 < len(lst2):
+
+        if lst1[idx1] <= lst2[idx2]:
+            idx1 += 1
+        else:
+            lst1.insert(idx1, lst2[idx2])
+            idx1 += 1
+            idx2 += 1
+
+    if idx2 < len(lst2):
+        lst1 += lst2[idx2:]
+
+    return lst1
+
+
+"""
+- Tradeoffs
+    - Merging in place reduces space but increases time complexity
+
+"""
+
 print(merge([1, 3, 4, 5], [2, 6, 7, 8]))
 print(merge([1, 3, 4], [2, 6, 8, 9, 10]))
 print(merge([1, 3, 3, 4], [2, 2, 7, 11]))
+print()
+print(merge_inplace([1, 3, 4, 5], [2, 6, 7, 8]))
+print(merge_inplace([1, 3, 4], [2, 6, 8, 9, 10]))
+print(merge_inplace([1, 3, 3, 4], [2, 2, 7, 11]))
