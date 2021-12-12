f73cd72adfdf672fc5f1f06f1f4e27b6e27cd9a2
Ty <tylertorresp@gmail.com>
2021-12-12 14:06:09 -0500

    Solution for variation of problem

--- educative/patterns/two_pointers/remove_duplicates.py
+++ educative/patterns/two_pointers/remove_duplicates.py
@@ -35,9 +35,25 @@ def execute(sorted_arr):
     return next_non_duplicate
 
 
+# Given an unsorted array of numbers and a target, remove all instances of target in place
+# O(N) T | O(1) S
+def execute_problem_variation(arr, target):
+    next_non_target = 0
+
+    for i in range(len(arr)):
+        if arr[i] != target:
+            arr[next_non_target] = arr[i]
+            next_non_target += 1
+
+    return next_non_target
+
+
 if __name__ == '__main__':
-    print(execute_optimal([2, 3, 3, 3, 6, 9, 9]))
-    print(execute_optimal([2, 2, 2, 11]))
-    print(execute_optimal_two([2, 3, 3, 3, 6, 9, 9]))
-    print(execute([2, 2, 2, 11]))
-    print(execute([2, 2, 2, 11, 13, 13, 13, 13, 14, 15, 16]))
+    # print(execute_optimal([2, 3, 3, 3, 6, 9, 9]))
+    # print(execute_optimal([2, 2, 2, 11]))
+    # print(execute_optimal_two([2, 3, 3, 3, 6, 9, 9]))
+    # print(execute([2, 2, 2, 11]))
+    # print(execute([2, 2, 2, 11, 13, 13, 13, 13, 14, 15, 16]))
+
+    print(execute_problem_variation([3, 2, 3, 6, 3, 10, 9, 3], 3))
+    print(execute_problem_variation([2, 11, 2, 2, 1], 2))
