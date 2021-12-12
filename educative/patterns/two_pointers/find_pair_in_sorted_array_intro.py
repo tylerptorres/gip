0a4b1ad660b2dd24f22af4ece3b7862fde53db95
Ty <tylertorresp@gmail.com>
2021-12-12 10:34:02 -0500

    Applying two pointers technique where we increment or decrement the pointers depending on the current sum of the values

---
+++ educative/patterns/two_pointers/find_pair_in_sorted_array_intro.py
@@ -0,0 +1,19 @@
+
+def execute_optimal(sorted_arr, target):
+    left, right = 0, len(sorted_arr) - 1
+
+    while left < right:
+        current_sum = sorted_arr[left] + sorted_arr[right]
+        if current_sum == target:
+            return [sorted_arr[left], sorted_arr[right]]
+
+        if current_sum < target:
+            left += 1
+        else:
+            right -= 1
+
+    return -1
+
+
+if __name__ == '__main__':
+    print(execute_optimal([1, 2, 3, 4, 6], 6))
