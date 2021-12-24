37587a5232d0573e73997615c6f1e7cb6bc53eb9
Ty <tylertorresp@gmail.com>
2021-12-23 20:41:59 -0500

    Updated solution to be more optimal

--- educative/patterns/tree_dfs/count_paths_for_sum.py
+++ educative/patterns/tree_dfs/count_paths_for_sum.py
@@ -7,12 +7,34 @@ O(4^k) T | O(N) S
 O(4^k) == O(N^2) where N = 2^k
  """
 
-
+# O(N^2) T | O(N) S
 def execute(root, path_sum):
     if root is None:
         return 0
 
-    return dfs(root, path_sum)
+    return dfs_optimal(root, path_sum)
+
+
+def dfs_optimal(root, path_sum, current=[]):
+    if root is None:
+        return 0
+
+    total_paths = 0
+    current_sum = path_sum
+    current.append(root.val)
+
+    for i in range(len(current) - 1, -1, -1):
+        current_sum -= current[i]
+
+        if current_sum == 0:
+            total_paths += 1
+
+    total_paths += dfs_optimal(root.left, path_sum, current) + dfs_optimal(
+        root.right, path_sum, current
+    )
+    current.pop()
+
+    return total_paths
 
 
 def dfs(root, path_sum):
@@ -29,10 +51,15 @@ def dfs(root, path_sum):
     including_right_parent = dfs(root.right, path_sum - root.val)
     excluding_right_parent = dfs(root.right, path_sum)
 
-    return including_left_parent + including_right_parent + excluding_left_parent + excluding_right_parent
+    return (
+        including_left_parent
+        + including_right_parent
+        + excluding_left_parent
+        + excluding_right_parent
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     root = TreeNode(12)
     root.left = TreeNode(7)
